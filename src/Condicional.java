f3cc0de7926ba5fc1d87edf97a51d1e943acc20a
VitorLima <vitorgl@ciandt.com>
2023-07-31 12:58:49 -0300

    first commit

--- src/Condicional.java
+++ src/Condicional.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Condicional {
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 1990;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
+
+        if (anoDeLancamento >= 2022){
+            System.out.println("Lançamento que os clientes estão curtindo");
+        } else {
+            System.out.println("Filme retrô que vale a pena asssitir!");
+        }
+
+        if (incluidoNoPlano == true || tipoPlano.equals("plus")){
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Deve pagar a loca Locação");
+        }
+    }
 }
